47e0467f6a256bf499d3fe6975085728d4149890
Gerrit Alex <admin@gerritalex.de>
2020-01-11 15:34:55 +0100

    protect hidden env in runtime

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,11 +1,21 @@
+const createKey = key => `REACT_APP_${key}`;
+
+const variableExistsInEnv = key =>
+  process.env[key] && process.env[key].length > 0;
+
+const throwErrorForMissingEnv = key => {
+  throw new Error(`missing env variable: ${key}`);
+};
+
 const requiredEnvVariables = [
   'REPO_LINK',
   'BRAND_NAME',
   'SITE_URL',
   'ENABLED_LANGUAGES',
   'ENABLED_PROVIDER',
-  'CRON_TOKEN',
 ];
+
+const requiredButHiddenVariables = ['CRON_TOKEN'];
 
 const optionalEnvVariables = [
   'SENTRY_DSN',
@@ -14,14 +24,22 @@ const optionalEnvVariables = [
   'DISCORD_LINK',
 ];
 
+requiredButHiddenVariables.forEach(envName => {
+  const key = createKey(envName);
+
+  if (!variableExistsInEnv(key)) {
+    throwErrorForMissingEnv(key);
+  }
+});
+
 export default [...requiredEnvVariables, ...optionalEnvVariables].reduce(
   (carry, envName) => {
-    const key = `REACT_APP_${envName}`;
+    const key = createKey(envName);
 
-    if (process.env[key] && process.env[key].length > 0) {
+    if (variableExistsInEnv(key)) {
       carry[envName] = process.env[key];
     } else if (requiredEnvVariables.includes(envName)) {
-      throw new Error(`missing env variable: ${key}`);
+      throwErrorForMissingEnv(key);
     }
 
     return carry;
