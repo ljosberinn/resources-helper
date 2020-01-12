061b9122dfc2bc308c871856cc51eb601dc80b4a
Gerrit Alex <admin@gerritalex.de>
2020-01-12 18:50:06 +0100

    fix leftover hook import

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,5 @@
 import useDetectColorScheme from './useDetectColorScheme';
 import useTheme from './useTheme';
 import useUser from './useUser';
-import useAbortSignal from './useAbortSignal';
 
-export { useDetectColorScheme, useTheme, useUser, useAbortSignal };
+export { useDetectColorScheme, useTheme, useUser };
