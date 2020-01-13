33e33f5a48bd009f675b594cfb0e99d033fe17a4
Gerrit Alex <admin@gerritalex.de>
2020-01-13 21:12:23 +0100

    remove preliminary MinesRoute

--- src/routes/private/index.js
+++ src/routes/private/index.js
@@ -8,7 +8,4 @@ export const PRIVATE_ROUTES = {
   [ROUTES.SETTINGS.routerPath]: LoadableComponent(() =>
     import(/* webpackChunkName: "private.settings" */ './SettingsRoute'),
   ),
-  [ROUTES.MINES.routerPath]: LoadableComponent(() =>
-    import(/* webpackChunkName: "private.mines" */ './MinesPage'),
-  ),
 };
